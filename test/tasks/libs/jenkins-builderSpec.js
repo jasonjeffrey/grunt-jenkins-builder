fc47460ca85bb449e66d0cb51043b3315d0184d4
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2015-03-31 16:25:52 +0100

    update location of files

--- test/tasks/libs/jenkins-builderSpec.js
+++ test/tasks/libs/jenkins-builderSpec.js
@@ -6,7 +6,7 @@ describe('jenkins-builder task',function () {
   describe('initialisation', function () {
 
     beforeEach(function () {
-        task = require('../../tasks/jenkins-builder.js');
+        task = require('../../../tasks/libs/jenkins-builder.js');
     });
 
     it('should not be undefined', function () {
