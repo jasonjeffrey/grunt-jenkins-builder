2f3b031e94d86864c838673a599c5711c0431c2f
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2015-04-27 15:05:46 +0100

    add async to the grunt task

--- tasks/libs/jenkins-runner.js
+++ tasks/libs/jenkins-runner.js
@@ -4,11 +4,14 @@ var jenkins,
     jenkinsapi = require('jenkins-api');
 
 exports.startJenkinsJob = function (grunt, options) {
+  done = this.async();
   jenkins = jenkinsapi.init(options.jenkinsUrl);
 
   jenkins.build(options.jobName, options.parameters, function (err) {
       if(err) {
         grunt.fail.fatal(err);
       }
+
+    done();
   });
 };
