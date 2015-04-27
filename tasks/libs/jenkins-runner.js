1bfe76ebe63516498327bfe7ba1256c06ae15197
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2015-04-27 15:08:54 +0100

    remove async from helper file

--- tasks/libs/jenkins-runner.js
+++ tasks/libs/jenkins-runner.js
@@ -4,14 +4,11 @@ var jenkins,
     jenkinsapi = require('jenkins-api');
 
 exports.startJenkinsJob = function (grunt, options) {
-  done = this.async();
   jenkins = jenkinsapi.init(options.jenkinsUrl);
 
   jenkins.build(options.jobName, options.parameters, function (err) {
       if(err) {
         grunt.fail.fatal(err);
       }
-
-    done();
   });
 };
