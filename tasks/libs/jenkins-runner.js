64eefbd9a466c7ea40f5f545b7d974f7c15f6186
Ricky Clegg <me@rickyclegg.com>
2017-03-22 12:31:14 +0000

    Add auto option to jenkins runner

--- tasks/libs/jenkins-runner.js
+++ tasks/libs/jenkins-runner.js
@@ -3,18 +3,35 @@
 var jenkins,
     jenkinsapi = require('jenkins-api');
 
+function buildAuthUrl(url, username, password) {
+  var protocolRegExp = /https:\/\/|http:\/\//,
+      protocol = '';
+
+  if (url.indexOf('http') > -1) {
+    protocol = protocolRegExp.exec(url)[0];
+    url = url.replace(protocolRegExp, '');
+  }
+
+  return protocol + username + ':' + password + '@' + url;
+}
+
 exports.startJenkinsJob = function (grunt, options, callback) {
   var onComplete = function (err) {
-    if(err) {
+    if (err) {
       grunt.fail.fatal(err);
     }
 
     callback();
-  };
+  },
+  url = options.jenkinsUrl;
 
-  jenkins = jenkinsapi.init(options.jenkinsUrl);
+  if (options.auth) {
+    url = buildAuthUrl(url, options.auth.username, options.auth.password);
+  }
 
-  if(options.parameters) {
+  jenkins = jenkinsapi.init(url);
+
+  if (options.parameters) {
     jenkins.build(options.jobName, options.parameters, onComplete);
   } else {
     jenkins.build(options.jobName, onComplete);
